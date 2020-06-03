37bce015e5ee609821d45d6618ae52eaca8d0d7e
Robert Walling <40674581+RWalling21@users.noreply.github.com>
2020-06-03 03:41:50 -0400

    First working build of NN

---
+++ convNN.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import numpy as np
+import pickle
+import time
+
+#TODO 
+# This section is more about what we need to learn to understand the NN better, and boy do we have a lot
+# How does a ConvNN Learn?
+# What exactly is Kernal Size and why is it important? 
+# How does Pooling work
+# What is Input Shape and how is conv shape defined
+# What is Conv2D padding and how can it improve my model
+# What is Dropout and how can it improve my model
+# Why do we use Sigmoid for our Output layer instead of Rectified Linear?
+# What is Binary Crossentropy?
+# How does the adam optimizer work, and what are other optimizers that might work better
+# Learn more about different conv architectures other than VGG
+# Better understanding of Features and Labels (Very simple)
+# What does the metrics property in the Compile function do
+# Is there a better way to process and store my training data
+
+# --- Variable ---
+
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+X = np.array(X/255.0)
+y = np.array(y)
+
+#Makes and runs multiple networks so that we can see which works best
+
+NAME = "VGG-convNN-{}".format(int(time.time())) #Formats the name of the Network to show which is most effective
+tensorboard = TensorBoard(log_dir="logs\\{}".format(NAME))
+print(NAME)
+
+model = Sequential()
+
+# Input Layer
+
+model.add(Conv2D(32, (3,3), activation="relu", input_shape = X.shape[1:])) # Set's up first Conv layer with a kernel size of 3 - 3
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Hidden Layers
+
+model.add(Conv2D(64, (3,3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3,3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation="relu")) # Leads to 10% increase in Validation loss but 5% decrease in training loss
+
+# Output Layer
+
+model.add(Dense(1, activation="sigmoid"))
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.2, callbacks=[tensorboard])
