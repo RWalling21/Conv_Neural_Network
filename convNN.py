1a4b137fde78d12490898874c0cec18f100c6ebe
Robert Walling <robert.walling@cttech.org>
2020-06-10 15:13:51 -0400

    Implemented Dropout

--- convNN.py
+++ convNN.py
@@ -14,15 +14,15 @@ import time
 # How does Pooling work
 # What is Input Shape and how is conv shape defined
 # What is Conv2D padding and how can it improve my model
+# What is a Kernel Initializer and what is it used for
 # Why do we use Sigmoid for our Output layer instead of Rectified Linear?
 # What is Binary Crossentropy?
-# How does the adam optimizer work, and what are other optimizers that might work better
 # Learn more about different conv architectures other than VGG
 # What does the metrics property in the Compile function do
 
 # --- In Progress ---
 
-# What is Dropout and how can it improve my model
+# use ImageDataGenerator to implement data augmentation
 
 # --- Variables ---
 
@@ -44,22 +44,26 @@ model = Sequential()
 
 model.add(Conv2D(32, (3,3), activation="relu", input_shape=(75, 75, 1))) # Set's up first Conv layer with a kernel size of 3 - 3
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
 # Hidden Layers
 
 model.add(Conv2D(64, (3,3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
 model.add(Conv2D(128, (3,3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
 
-model.add(Dense(128, activation="relu")) # Leads to 10% increase in Validation loss but 5% decrease in training loss
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.5))
 
 # Output Layer
 
 model.add(Dense(1, activation="sigmoid"))
 
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(X, y, batch_size=32, epochs=10, validation_split=0.2, callbacks=[tensorboard])
+model.fit(X, y, batch_size=32, epochs=35, validation_split=0.2, callbacks=[tensorboard])
